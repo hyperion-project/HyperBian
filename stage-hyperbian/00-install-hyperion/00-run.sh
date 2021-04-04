4782ee620780c2c06d9cd41c9d6cf20308a8b1a3
Paulchen Panther <Paulchen-Panter@protonmail.com>
2021-04-04 21:09:34 +0200

    Run Hyperion as pi user/Custom MOTD message/New Readme.md image

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -9,12 +9,9 @@ HYPERION_LATEST_VERSION=$(curl -sL "$HYPERION_RELEASES_URL" | grep "tag_name" | 
 HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Hyperion-$HYPERION_LATEST_VERSION-Linux-armv6l.deb
 
 # Download latest release
-echo '           Download Hyperion'
+echo 'Downloading Hyperion ........................'
 mkdir -p "$ROOTFS_DIR"/tmp
 curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/hyperion.deb
-
-# Copy service file
-cp hyperion.service ${ROOTFS_DIR}/etc/systemd/system/hyperion.service
 
 # Enable SPI and force HDMI output
 sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
@@ -24,14 +21,23 @@ sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/con
 sed -i "s/Raspbian/HyperBian/gI" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/www.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/forum.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/forum.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+
+# Custom motd
+rm "${ROOTFS_DIR}"/etc/motd
+rm "${ROOTFS_DIR}"/etc/update-motd.d/10-uname
+install -m 755 files/motd-hyperbian "${ROOTFS_DIR}"/etc/update-motd.d/10-hyperbian
+
+# Remove the "last login" information
+sed -i "s/^#PrintLastLog yes.*/PrintLastLog no/" ${ROOTFS_DIR}/etc/ssh/sshd_config
 
 on_chroot << EOF
-echo '           Install Hyperion'
+echo 'Installing Hyperion ........................'
 apt install /tmp/hyperion.deb
-rm -f /etc/systemd/system/hyperiond@.service
-echo '           Register Hyperion'
-systemctl -q enable hyperion.service
+rm /tmp/hyperion.deb
+mv /etc/systemd/system/hyperiond@.service /etc/systemd/system/hyperion@.service
+echo 'Registering Hyperion ........................'
+systemctl -q enable hyperion"@pi".service
 EOF
