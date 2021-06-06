997de4580d63a490edc03ae025e41d79cdb535bb
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2021-06-06 14:43:12 +0200

    install hyperion from APT server

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-# Select the appropriate download path
-HYPERION_DOWNLOAD_URL="https://github.com/hyperion-project/hyperion.ng/releases/download"
-HYPERION_RELEASES_URL="https://api.github.com/repos/hyperion-project/hyperion.ng/releases"
-
-# Get the latest version
-HYPERION_LATEST_VERSION=$(curl -sL "$HYPERION_RELEASES_URL" | grep "tag_name" | head -1 | cut -d '"' -f 4)
-HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Hyperion-$HYPERION_LATEST_VERSION-Linux-armv6l.deb
-
-# Download latest release
-echo 'Downloading Hyperion ........................'
-mkdir -p "$ROOTFS_DIR"/tmp
-curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/hyperion.deb
-
 # Enable SPI and force HDMI output
 sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
 sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
@@ -34,10 +21,15 @@ install -m 755 files/motd-hyperbian "${ROOTFS_DIR}"/etc/update-motd.d/10-hyperbi
 sed -i "s/^#PrintLastLog yes.*/PrintLastLog no/" ${ROOTFS_DIR}/etc/ssh/sshd_config
 
 on_chroot << EOF
-echo 'Installing Hyperion ........................'
-apt install /tmp/hyperion.deb
-rm /tmp/hyperion.deb
+echo '---> Import the public GPG key from the APT server into HyperBian'
+wget -qO - https://apt.hyperion-project.org/hyperion.pub.key | apt-key add -
+echo '---> Add Hyperion to the APT sources'
+echo "deb https://apt.hyperion-project.org/ stable main" > /etc/apt/sources.list.d/hyperion.list
+echo '---> Update the APT sources'
+apt-get update
+echo '---> Installing Hyperion'
+apt-get -y install hyperion
+echo '---> Registering Hyperion'
 mv /etc/systemd/system/hyperiond@.service /etc/systemd/system/hyperion@.service
-echo 'Registering Hyperion ........................'
 systemctl -q enable hyperion"@pi".service
 EOF
