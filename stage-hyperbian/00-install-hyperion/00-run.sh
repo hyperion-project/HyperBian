73aa91249ee9e12c27a10cb36c0761018dcfe8e0
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2023-11-18 16:00:04 +0000

    add 64bit HyperBian

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -4,10 +4,13 @@
 sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
 sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
 
+# Determine Hyperion Version
+HYPERION_VERSION=$(curl -sL "https://github.com/hyperion-project/hyperion.ng/raw/master/.version")
+
 # Modify /usr/lib/os-release
 sed -i "s/Raspbian/HyperBian/gI" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
+sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
@@ -16,7 +19,9 @@ sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/hyperion-project.org\/\
 rm "${ROOTFS_DIR}"/etc/motd
 rm "${ROOTFS_DIR}"/etc/update-motd.d/10-uname
 install -m 755 files/motd-hyperbian "${ROOTFS_DIR}"/etc/update-motd.d/10-hyperbian
-install -m 644 files/hyperion.sources ${ROOTFS_DIR}/usr/share/keyrings/
+
+# DEB822 source file
+install -m 644 files/hyperion.sources ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
 # Remove the "last login" information
 sed -i "s/^#PrintLastLog yes.*/PrintLastLog no/" ${ROOTFS_DIR}/etc/ssh/sshd_config
