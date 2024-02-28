e2feeb3af1451f7e235fbb3fed14ef7be8bb7657
LordGrey <48840279+Lord-Grey@users.noreply.github.com>
2024-02-28 19:33:44 +0100

    New firmware directory location

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -2,8 +2,8 @@
 
 # Enable SPI and force HDMI output
 echo '---> Enable SPI and force HDMI output'
-sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
-sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/config.txt
+sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/firmware/config.txt
+sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/firmware/config.txt
 
 # Modify /usr/lib/os-release
 echo '---> Customize HyperBian'
